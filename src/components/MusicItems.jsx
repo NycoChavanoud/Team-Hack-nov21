a9fac49b00da0f887e9a71e0f90e5da768f912f4
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-11-25 23:31:31 +0100

    responsive on react player fixed

--- src/components/MusicItems.jsx
+++ src/components/MusicItems.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import "./css/MusicItem.css";
 
-import ReactPlayer from "react-player";
+import ReactPlayer from "react-player/youtube";
 
 const MusicItems = ({ songName, artist, linkKaraoke, jacket }) => {
   // const [isFavorite, setIsFavorite] = useState(false);
+
   const [displayPopup, setDisplayPopup] = useState(false);
 
   // function handleClickfavorite() {
@@ -32,7 +33,13 @@ const MusicItems = ({ songName, artist, linkKaraoke, jacket }) => {
           style={{ display: displayPopup ? "block" : "none" }}
         >
           <button onClick={() => setDisplayPopup(false)}>X</button>
-          <ReactPlayer url={linkKaraoke} controls={true} />
+          <ReactPlayer
+            url={linkKaraoke}
+            className="react-player"
+            height="90vh"
+            width="auto"
+            controls={true}
+          />
         </div>
       </div>
       {/* <div id="favorite" onClick={handleClickfavorite}>
