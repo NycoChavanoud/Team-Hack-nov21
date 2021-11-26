2cb36dfdf05b4434721e9f4e8f754319e246c7d2
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-11-26 01:29:15 +0100

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./css/Navbar.css";
 import home from "../assets/note.png";
 import micro from "../assets/micro.png";
 import play from "../assets/play.png";
@@ -7,33 +8,22 @@ import cd from "../assets/cd.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <ul>
-        <NavLink to="/">
-          <a href="home" className="home">
-            <img className="" src={home} alt="home" />
-          </a>
-          Home
-        </NavLink>
-        <NavLink to="/karaoke" className="">
-          <a href="karaoke" className="">
-            <img className="micro" src={micro} alt="micro" />
-          </a>
-          Karaoké
-        </NavLink>
-        <NavLink to="/playlist" className="">
-          <a href="playlist" className="playlist">
-            <img className="play" src={play} alt="playlist" />
-          </a>
-          Playlist
-        </NavLink>
-        <NavLink to="/blind-test" className="">
-          <a href="blind-test" className="blind-test">
-            <img className="blind-test" src={cd} alt="blind-test" />
-          </a>
-          Blind Test
-        </NavLink>
-      </ul>
+    <div className="navbar-container">
+      <NavLink to="/">
+        <img className="logo-home" src={home} alt="home" />
+      </NavLink>
+
+      <NavLink to="/karaoke-list">
+        <img className="logo-karaoke" src={micro} alt="logo-micro" />
+      </NavLink>
+
+      <NavLink to="/playlist">
+        <img className="logo-playlist" src={play} alt="playlist" />
+      </NavLink>
+
+      <NavLink to="/blind-test">
+        <img className="logo-blind-test" src={cd} alt="blind-test" />
+      </NavLink>
     </div>
   );
 };
