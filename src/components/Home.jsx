db3f78af7116e5cdec9b0b3f374bb83dbbf4a44c
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-11-26 02:09:13 +0100

    home page ok, need find good text and picture

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./css/Home.css";
 import logo from "../assets/logo.png";
+import provisoire from "../assets/home.png";
 
 function Home() {
   return (
     <>
-      <img src={logo} alt="Logo" />
+      <img id="logo-homepage" src={logo} alt="Logo" />
+      <div className="container-home-page">
+        <h1 id="title-home-page">Saturday Night App'</h1>
+        <p id="baseline-home-page"> Dansez, chantez, jouez...</p>
+        <img src={provisoire} id="photo-home-page" alt="fete" />
+        <p id="presentation-home-page">
+          Saturday night app met de la musique dans vos soirées!
+        </p>
+      </div>
     </>
   );
 }
