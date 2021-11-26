4faaa3fb46f7ee413c91e6b916c370546dd1d03c
Selena <selena.poun@gmail.com>
2021-11-26 01:21:49 +0100

    fin de nuit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,16 @@ import "./css/Home.css";
 import Navbar from "./Navbar";
 
 const Home = () => {
-  return <>accueil</>;
+  return (
+    <>
+      <h1>Bienvenue sur Saturday Night !</h1>
+      <p>
+        Chantez Choissisez vos musiques et chantez Créez vos propres playlist et
+        votez pour les musiques Testez vos connaissances en musique !
+      </p>
+    </>
+  );
 };
 
 export default Home;
+partagées;
